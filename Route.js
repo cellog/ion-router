cee6d88c4a2a10e5e58f1e66fec3aaa74c6ef083
Gregory Beaver <greg@chiaraquartet.net>
2017-01-17 15:20:45 -0600

    fix 2 issues, ignore a bunch of others

--- Route.js
+++ Route.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/Routes.js')
+module.exports = require('./lib/Route.js')
--- Routes.js
+++ Routes.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/Route.js')
+module.exports = require('./lib/Routes.js')
