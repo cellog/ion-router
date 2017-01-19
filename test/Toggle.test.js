a1fa5c0165b6e23503fc7e0cc95a03ae0ef91d76
Gregory Beaver <greg@chiaraquartet.net>
2017-01-18 22:55:40 -0600

    fix test

--- test/Toggle.test.js
+++ test/Toggle.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Toggle from '../src/Toggle'
+import Toggle, { connectToggle } from '../src/Toggle'
 import DisplaysChildren from '../src/DisplaysChildren'
-import { renderComponent } from './test_helper'
+import { renderComponent, connect } from './test_helper'
 
 describe('Toggle', () => {
   const Component = props => ( // eslint-disable-next-line
@@ -13,74 +13,85 @@ describe('Toggle', () => {
   beforeEach(() => {
     Route = Toggle((s, p) => s.week || p.week)
   })
-  it('renders the component if the state tester returns true', () => {
-    const container = renderComponent(Route, { component: Component, foo: 'bar' }, { week: 1 })
-    expect(container.find(Component)).has.length(1)
-    expect(container.find('.foo')).has.length(1)
-    expect(container.find('.foo').text()).eqls('bar')
+  it('should freak out if we don\'t initialize', () => {
+    expect(() => renderComponent(Route, { component: Component, foo: 'bar' }, { week: 1 }))
+      .throws('call connectToggle with the connect function from react-redux to ' +
+      'initialize Toggle (see https://github.com/cellog/react-redux-saga-router/issues/1)')
   })
-  it('renders the component if the state tester returns true from props', () => {
-    const container = renderComponent(Route, { component: Component, foo: 'bar', week: 1 }, { week: 0 })
-    expect(container.find(Component)).has.length(1)
-    expect(container.find('.foo')).has.length(1)
-    expect(container.find('.foo').text()).eqls('bar')
-  })
-  it('renders null if the state tester returns false', () => {
-    const container = renderComponent(Route, { component: Component, foo: 'bar' }, { week: 0 })
-    expect(container.find(Component)).has.length(0)
-    expect(container.text()).eqls('')
-  })
-  it('renders else if the state tester returns false', () => {
-    const Else = () => <div>else</div>
-    const container = renderComponent(Route, { component: Component, else: Else }, { week: 0 })
-    expect(container.find(Component)).has.length(0)
-    expect(container.text()).eqls('else')
-  })
-  it('does not call state if loaded returns false', () => {
-    const spy = sinon.spy(() => true)
-    const loaded = sinon.spy(() => false)
-    const R = Toggle(spy, loaded)
-    const container = renderComponent(R, { component: Component, foo: 'bar', week: 1 }, { week: 0 })
-
-    expect(spy.called).is.false
-    expect(loaded.called).is.true
-    expect(container.find(Component)).has.length(0)
-  })
-  it('renders loading element if state is still loading', () => {
-    const R = Toggle(() => true, () => false)
-    const container = renderComponent(R,
-      { component: Component, loadingComponent: () => <div>Loading...</div> })
-    expect(container.find(Component)).has.length(0)
-    expect(container.text()).eqls('Loading...')
-  })
-  it('componentLoadingMap', () => {
-    const R = Toggle(() => true, () => true, {
-      component: 'bobby',
-      loadingComponent: 'frenzel',
-      else: 'blah'
+  describe('initialized', () => {
+    beforeEach(() => {
+      connectToggle(connect)
+      Route = Toggle((s, p) => s.week || p.week)
     })
-    const container = renderComponent(R, { component: Component, bobby: 'hi', frenzel: 'there', blah: 'oops' })
-    expect(container.find(Component)).has.length(1)
-    expect(container.find(Component).props('component')).eqls('hi')
-    expect(container.find(Component).props('bobby')).eqls(undefined)
-    expect(container.find(Component).props('loadingComponent')).eqls('there')
-    expect(container.find(Component).props('frenzel')).eqls(undefined)
-    expect(container.find(Component).props('else')).eqls('oops')
-    expect(container.find(Component).props('blah')).eqls(undefined)
-  })
-  it('no specified component', () => {
-    const R = Toggle(() => true, () => true)
-    const J = () => <R>
-      testing
-    </R>
-    const container = renderComponent(J)
-    expect(container.find(DisplaysChildren)).has.length(1)
-    expect(container.text()).eqls('testing')
-  })
-  it('renders children', () => {
-    const R = Toggle(() => true, () => true)
-    const J = props => <R> {props.children}</R> // eslint-disable-line
-    const container = renderComponent(J, { children: <ul><li>hi</li><li>there</li></ul> })
-    expect(container.text()).eqls(' hithere')
+    it('renders the component if the state tester returns true', () => {
+      const container = renderComponent(Route, { component: Component, foo: 'bar' }, { week: 1 })
+      expect(container.find(Component)).has.length(1)
+      expect(container.find('.foo')).has.length(1)
+      expect(container.find('.foo').text()).eqls('bar')
+    })
+    it('renders the component if the state tester returns true from props', () => {
+      const container = renderComponent(Route, { component: Component, foo: 'bar', week: 1 }, { week: 0 })
+      expect(container.find(Component)).has.length(1)
+      expect(container.find('.foo')).has.length(1)
+      expect(container.find('.foo').text()).eqls('bar')
+    })
+    it('renders null if the state tester returns false', () => {
+      const container = renderComponent(Route, { component: Component, foo: 'bar' }, { week: 0 })
+      expect(container.find(Component)).has.length(0)
+      expect(container.text()).eqls('')
+    })
+    it('renders else if the state tester returns false', () => {
+      const Else = () => <div>else</div>
+      const container = renderComponent(Route, { component: Component, else: Else }, { week: 0 })
+      expect(container.find(Component)).has.length(0)
+      expect(container.text()).eqls('else')
+    })
+    it('does not call state if loaded returns false', () => {
+      const spy = sinon.spy(() => true)
+      const loaded = sinon.spy(() => false)
+      const R = Toggle(spy, loaded)
+      const container = renderComponent(R, { component: Component, foo: 'bar', week: 1 }, { week: 0 })
+
+      expect(spy.called).is.false
+      expect(loaded.called).is.true
+      expect(container.find(Component)).has.length(0)
+    })
+    it('renders loading element if state is still loading', () => {
+      const R = Toggle(() => true, () => false)
+      const container = renderComponent(R,
+        { component: Component, loadingComponent: () => <div>Loading...</div> })
+      expect(container.find(Component)).has.length(0)
+      expect(container.text()).eqls('Loading...')
+    })
+    it('componentLoadingMap', () => {
+      const R = Toggle(() => true, () => true, {
+        component: 'bobby',
+        loadingComponent: 'frenzel',
+        else: 'blah'
+      })
+      const container = renderComponent(R, { component: Component, bobby: 'hi', frenzel: 'there', blah: 'oops' })
+      expect(container.find(Component)).has.length(1)
+      expect(container.find(Component).props('component')).eqls('hi')
+      expect(container.find(Component).props('bobby')).eqls(undefined)
+      expect(container.find(Component).props('loadingComponent')).eqls('there')
+      expect(container.find(Component).props('frenzel')).eqls(undefined)
+      expect(container.find(Component).props('else')).eqls('oops')
+      expect(container.find(Component).props('blah')).eqls(undefined)
+    })
+    it('no specified component', () => {
+      const R = Toggle(() => true, () => true)
+      const J = () => <R>
+        testing
+      </R>
+      const container = renderComponent(J)
+      expect(container.find(DisplaysChildren)).has.length(1)
+      expect(container.text()).eqls('testing')
+    })
+    it('renders children', () => {
+      const R = Toggle(() => true, () => true)
+      const J = props => <R> {props.children}</R> // eslint-disable-line
+      const container = renderComponent(J, { children: <ul><li>hi</li><li>there</li></ul> })
+      expect(container.text()).eqls(' hithere')
+    })
   })
 })
